969b0f20c46f1c6b6cd40c756672664302a1c8f4
Lucas Hauszler <lucas.hauszler@epitech.eu>
2024-12-10 13:46:29 +0100

    feat(game): make camera less goofy and move according to user lookat

--- src/game/MovePlayer.cpp
+++ src/game/MovePlayer.cpp
@@ -4,26 +4,26 @@
 #include "SoftBodyBox.hpp"
 #include "SoftBodyNode.hpp"
 
-static glm::vec3 GetPlayerMovement()
+static glm::vec3 GetPlayerMovement(const glm::vec3& cameraForward, const glm::vec3& cameraRight)
 {
     constexpr static float MOVEMENT_FORCE = 20.f;
     glm::vec3 movement = {0, 0, 0};
 
     if (Raylib::IsKeyDown(Raylib::KEY_W))
     {
-        movement.z += MOVEMENT_FORCE;
+        movement += cameraForward * MOVEMENT_FORCE;
     }
     if (Raylib::IsKeyDown(Raylib::KEY_S))
     {
-        movement.z -= MOVEMENT_FORCE;
+        movement -= cameraForward * MOVEMENT_FORCE;
     }
     if (Raylib::IsKeyDown(Raylib::KEY_A))
     {
-        movement.x += MOVEMENT_FORCE;
+        movement -= cameraRight * MOVEMENT_FORCE;
     }
     if (Raylib::IsKeyDown(Raylib::KEY_D))
     {
-        movement.x -= MOVEMENT_FORCE;
+        movement += cameraRight * MOVEMENT_FORCE;
     }
 
     return movement;
@@ -31,7 +31,32 @@ static glm::vec3 GetPlayerMovement()
 
 void Game::MovePlayer(ES::Engine::Registry& registry)
 {
-    auto movement = GetPlayerMovement();
+    auto cameraView = registry.GetRegistry().view<Raylib::Camera3D>();
+
+    if (cameraView.size() != 1)
+    {
+        return;
+    }
+
+    glm::vec3 cameraPosition;
+    glm::vec3 cameraForward;
+    glm::vec3 cameraRight;
+
+    for (auto entity : cameraView)
+    {
+        auto &camera = cameraView.get<Raylib::Camera3D>(entity);
+        cameraPosition = {camera.position.x, camera.position.y, camera.position.z};
+
+        glm::vec3 cameraLookAt = {camera.target.x, camera.target.y, camera.target.z};
+        glm::vec3 fullForward = glm::normalize(cameraLookAt - cameraPosition);
+
+        cameraForward = glm::normalize(glm::vec3(fullForward.x, 0.f, fullForward.z));
+
+        glm::vec3 worldUp = {0.f, 1.f, 0.f};
+        cameraRight = glm::normalize(glm::cross(cameraForward, worldUp));
+    }
+
+    auto movement = GetPlayerMovement(cameraForward, cameraRight);
     auto view = registry.GetRegistry().view<Game::PlayerTag, Objects::SoftBodyBox>();
 
     for (auto entity : view)
@@ -43,5 +68,7 @@ void Game::MovePlayer(ES::Engine::Registry& registry)
             auto &node = registry.GetRegistry().get<ES::Plugin::Physics::Component::SoftBodyNode>(particle);
             node.ApplyForce(movement);
         }
+
+        
     }
 }
--- src/game/UpdateCamera.cpp
+++ src/game/UpdateCamera.cpp
@@ -18,19 +18,32 @@ void Game::UpdateCamera(ES::Engine::Registry &registry)
         gVelocity = box.GetVelocity(registry);
     }
 
+    static float angleX = 0.0f;
+    static float angleY = 0.0f;
+    const float sensitivity = 0.005f;
+
+    auto md = Raylib::GetMouseDelta();
+    float mouseDeltaX = md.x;
+    float mouseDeltaY = md.y;
+
+    angleX += mouseDeltaX * sensitivity;
+    angleY += mouseDeltaY * sensitivity;
+
+    const float maxVerticalAngle = glm::radians(89.0f);
+    angleY = glm::clamp(angleY, -maxVerticalAngle, maxVerticalAngle);
+
+    float distance = 8.0f;
+    glm::vec3 offset;
+    offset.x = distance * cos(angleY) * sin(angleX);
+    offset.y = distance * sin(angleY);
+    offset.z = distance * cos(angleY) * cos(angleX);
+
     auto view2 = registry.GetRegistry().view<Raylib::Camera3D>();
-
-    if (glm::length(gVelocity) < 1) {
-        return;
-    }
-
-    gVelocity[1] = 0;
-    gVelocity = glm::normalize(gVelocity);
-
     for (auto entity : view2)
     {
         auto &camera = view2.get<Raylib::Camera3D>(entity);
-        camera.position = {middlePos.x, middlePos.y + 10, middlePos.z + 10};
         camera.target = {middlePos.x, middlePos.y, middlePos.z};
+        middlePos += offset;
+        camera.position = {middlePos.x, middlePos.y, middlePos.z};
     }
 }
